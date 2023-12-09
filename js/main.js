3220ad2ae045bf8179e96dd48f32f7ffa52eaa49
Rhara Ianna <rharaianna@gmail.com>
2023-12-08 21:46:30 -0300

    organiza arquivos

--- js/main.js
+++ js/main.js
@@ -1,27 +1,2 @@
-let imagens = [
-    "./assets/dado1.png", 
-    "./assets/dado2.png",
-    "./assets/dado3.png",
-    "./assets/dado4.png",
-    "./assets/dado5.png",
-    "./assets/dado6.png"
-];
+import {sorteia, colocaDadoNaColuna, placar, tiraDados} from "./operações.js"
 
-let dados = document.querySelectorAll(".dado>img"); 
-
-function rolaDado(){
-    dados.forEach(function (dd) {
-        dd.classList.add("shake");
-    });
-        setTimeout(function()
-            {
-                dados.forEach(function(dd){
-                    dd.classList.remove("shake");
-                });
-                let valordd1 = Math.floor (Math.random()*6);
-                
-                document.querySelector("#dd1").setAttribute("src",imagens[valordd1]);
-        },
-        1000
-        );
-};
